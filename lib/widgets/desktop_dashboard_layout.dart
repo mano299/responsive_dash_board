237cab30890890c6afeed76a21604fbff0922386
mano299 <mano299ahmed1@gmail.com>
2025-08-25 02:31:15 +0300

    create list tile item

--- lib/widgets/desktop_dashboard_layout.dart
+++ lib/widgets/desktop_dashboard_layout.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 
 class DesktopDashboardLayout extends StatelessWidget {
   const DesktopDashboardLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Expanded(child: CustomDrawer())
+      ],
+    );
   }
 }
