6e7fc6576dba34268e9bf4c0273cc8a21249adbb
mano299 <mano299ahmed1@gmail.com>
2025-08-24 20:55:55 +0300

    generate assets file

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.svg
+  static const String imagesAvatar = "assets/images/avatar.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCard
+  /// assets/images/card.svg
+  static const String imagesCard = "assets/images/card.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesFreind1
+  /// assets/images/freind_1.svg
+  static const String imagesFreind1 = "assets/images/freind_1.svg";
+
+  /// Assets for imagesFreind2
+  /// assets/images/freind_2.svg
+  static const String imagesFreind2 = "assets/images/freind_2.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesInvestment
+  /// assets/images/investment.svg
+  static const String imagesInvestment = "assets/images/investment.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatics
+  /// assets/images/statics.svg
+  static const String imagesStatics = "assets/images/statics.svg";
+
+  /// Assets for imagesTransaction
+  /// assets/images/transaction.svg
+  static const String imagesTransaction = "assets/images/transaction.svg";
+
+  /// Assets for imagesWallet
+  /// assets/images/wallet.svg
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
