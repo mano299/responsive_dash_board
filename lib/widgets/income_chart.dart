2258499218ae648214e6e272daabdcf96e00944b
mano299 <mano299ahmed1@gmail.com>
2025-08-28 18:39:41 +0300

    create icome chart

--- lib/widgets/income_chart.dart
+++ lib/widgets/income_chart.dart
@@ -1,22 +1,52 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class IncomeChart extends StatelessWidget {
+class IncomeChart extends StatefulWidget {
   const IncomeChart({super.key});
 
   @override
+  State<IncomeChart> createState() => _IncomeChartState();
+}
+
+class _IncomeChartState extends State<IncomeChart> {
+  
+  @override
   Widget build(BuildContext context) {
-    return AspectRatio(aspectRatio: 1, child: PieChart(getPieChartDat()));
+    return AspectRatio(
+      aspectRatio: 1,
+      child: PieChart(getPieChartDat()),
+    );
   }
 
   PieChartData getPieChartDat() {
-    return PieChartData(sections: [
-      PieChartSectionData(
-        showTitle: false,
-        value: 40,
-        color: Color(0xff208CC8),
-        radius: 40,
-      ),
-    ]);
+    return PieChartData(
+      sectionsSpace: 0,
+      sections: [
+        PieChartSectionData(
+          showTitle: false,
+          value: 40,
+          color: Color(0xff208CC8),
+          radius: 40,
+        ),
+        PieChartSectionData(
+          showTitle: false,
+          value: 25,
+          color: Color(0xff4EB7F2),
+          radius: 40,
+        ),
+        PieChartSectionData(
+          showTitle: false,
+          value: 20,
+          color: Color(0xff064061),
+          radius: 40,
+        ),
+        PieChartSectionData(
+          showTitle: false,
+          value: 22,
+          color: Color(0xffE2DECD),
+          radius: 40,
+        ),
+      ],
+    );
   }
 }
