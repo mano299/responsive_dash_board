0fad7d181bf02bde494ae46bd53c1f89a1f7be01
mano299 <mano299ahmed1@gmail.com>
2025-08-28 03:35:25 +0300

    create custom dot

---
+++ lib/widgets/custom_dot.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomDot extends StatelessWidget {
+  const CustomDot({super.key, required this.isActive});
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: 8,
+      width: isActive ? 32: 8,
+      decoration: ShapeDecoration(
+        color:isActive ? Color(0xff4EB7F2): Color(0xffE8E8E8),
+        shape: RoundedRectangleBorder(
+        ),
+      ),
+    );
+  }
+}
