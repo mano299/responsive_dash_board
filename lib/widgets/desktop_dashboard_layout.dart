09a2ffbd46d7920b7d5e6a3cc345d3b4d1b3429a
mano299 <mano299ahmed1@gmail.com>
2025-08-28 04:02:28 +0300

    finish my card section

--- lib/widgets/desktop_dashboard_layout.dart
+++ lib/widgets/desktop_dashboard_layout.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/dots_indicator.dart';
-import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
+import 'package:responsive_dash_board/widgets/my_cards_section.dart';
 
 class DesktopDashboardLayout extends StatelessWidget {
   const DesktopDashboardLayout({super.key});
@@ -19,14 +18,7 @@ class DesktopDashboardLayout extends StatelessWidget {
         ),
         SizedBox(width: 24),
         Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              MyCardsPageView(),
-              SizedBox(height: 19),
-              DotsIndicator()
-            ],
-          ),
+          child: MyCardsSection()
         ),
       ],
     );
--- lib/widgets/dots_indicator.dart
+++ lib/widgets/dots_indicator.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_dot.dart';
 
 class DotsIndicator extends StatelessWidget {
-  const DotsIndicator({super.key});
-
+  const DotsIndicator({super.key, required this.currentIndex});
+  final int currentIndex;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: List.generate(3, (index) => Padding(
         padding: const EdgeInsets.only(right: 6.0),
-        child: CustomDot(isActive: false),
+        child: CustomDot(isActive: index == currentIndex),
       ),),
     );
   }
