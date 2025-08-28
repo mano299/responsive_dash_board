5bf3d1c34ad96800573bdc34be8d0afbd3652ed4
mano299 <mano299ahmed1@gmail.com>
2025-08-28 07:00:49 +0300

    create transaction model

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/transaction_model.dart';
 import 'package:responsive_dash_board/utils/app_styles.dart';
 
 class TransactionItem extends StatelessWidget {
-  const TransactionItem({super.key});
-
+  const TransactionItem({super.key, required this.transactionModel});
+  final TransactionModel transactionModel;
   @override
   Widget build(BuildContext context) {
     return Card(
-          elevation: 0,
-          color: Color(0xffFAFAFA),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+      elevation: 0,
+      color: Color(0xffFAFAFA),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ListTile(
+        title: Text(
+          transactionModel.title,
+          style: AppStyles.styleSemiBold16,
+        ),
+        subtitle: Text(
+          transactionModel.date,
+          style: AppStyles.styleRegular16,
+        ),
+        trailing: Text(
+          '\$${transactionModel.amount}',
+          style: AppStyles.styleSemiBold20.copyWith(
+            color: transactionModel.isWithdraw ? Colors.red : Colors.green,
           ),
-          child: ListTile(
-            title: Text(
-              'Cash Withdrawal',
-              style: AppStyles.styleSemiBold16,
-            ),
-            subtitle: Text(
-              '13 Apr, 2022 ',
-              style: AppStyles.styleRegular16,
-            ),
-            trailing: Text(r'$20,129', style: AppStyles.styleSemiBold20.copyWith(color: Colors.red),),
-          ),
-        );
+        ),
+      ),
+    );
   }
 }
